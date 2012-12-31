4f92f867fa0c8582c25bd6e9debbbe7cd677930a
Sebastian Haas <dev@sebastianhaas.info>
2012-12-31 22:36:31 +0100

    tests: Add raw tests

---
+++ tests/test-raw_basic.js
@@ -0,0 +1,43 @@
+var can = require('can');
+
+exports['channel_creation'] = function(test) {
+	// Throw exception if channel doesn't exist
+	test.throws(function() { can.createRawChannel("non_existant_channel")});
+	
+	var channel = can.createRawChannel("vcan0");
+	
+	test.throws(function() { channel.stop(); });
+	
+	test.done();
+}
+
+// Send 100 messages from c2 to c1
+exports['rxtx_test'] = function(test) {
+	var c1 = can.createRawChannel("vcan0");
+	var c2 = can.createRawChannel("vcan0");
+	
+	c1.start();
+	c2.start();
+	
+	var canmsg = { id: 10, data: [ 0 ] };
+
+	var rx_count = 0;
+	
+	c1.addListener("onMessage", function(msg) {
+		test.equal(msg.data[0], rx_count);
+		rx_count++;
+	});
+
+	for (var i = 0; i < 100; i++) {
+		canmsg.data[0] = i;	
+		c2.send(canmsg);
+	}
+	
+	setTimeout(function() {
+		test.equals(rx_count, i);
+		c1.stop();
+		c2.stop();
+		
+		test.done();
+	}, 100);
+}
