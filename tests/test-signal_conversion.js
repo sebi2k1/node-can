60b3cc0c9ed0ec89d2c7bda5a93f71424ecf4079
Don Mahurin <@>
2016-01-11 16:13:10 -0800

    Update unit tests to match current setvalue/getvalue results, which have been verified Kayak extractBits code.

--- tests/test-signal_conversion.js
+++ tests/test-signal_conversion.js
@@ -12,32 +12,32 @@ console.log(data);
 console.log(data);
 	signals.encode_signal(data, 3, 1, true, false, 1);
 console.log(data);
-	test.deepEqual(data, new Buffer([0xD0, 0x00, 0x00, 0, 0, 0, 0, 0]));
+	test.deepEqual(data, new Buffer([0x0B, 0x00, 0x00, 0, 0, 0, 0, 0]));
 	signals.encode_signal(data, 4, 8, true, false, 0xEA);
-	test.deepEqual(data, new Buffer([0xDE, 0xA0, 0x00, 0, 0, 0, 0, 0]));
+	test.deepEqual(data, new Buffer([0xAB, 0x0E, 0x00, 0, 0, 0, 0, 0]));
 	
 	signals.encode_signal(data, 12, 12, true, false, 0xEDB);
-	test.deepEqual(data, new Buffer([0xDE, 0xAD, 0xBE, 0, 0, 0, 0, 0]));
+	test.deepEqual(data, new Buffer([0xAB, 0xBE, 0xED, 0, 0, 0, 0, 0]));
 
 	signals.encode_signal(data, 12, 12, true, false, 0);
-	test.deepEqual(data, new Buffer([0xDE, 0xA0, 0x00, 0, 0, 0, 0, 0]), "Overwriting signal value failed");
+	test.deepEqual(data, new Buffer([0xAB, 0x0E, 0x00, 0, 0, 0, 0, 0]), "Overwriting signal value failed");
 	test.done();
 }
 
 exports['little_endian_decode'] = function(test) {
 	data = new Buffer([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]);
-	
+
 	test.equals(signals.decode_signal(data, 0, 8, true, false), 0xDE);
-	test.equals(signals.decode_signal(data, 0, 12, true, false), 0xADE);
+	test.equals(signals.decode_signal(data, 0, 12, true, false), 0xDDE);
 	test.equals(signals.decode_signal(data, 0, 16, true, false), 0xADDE);
 	
-	test.equals(signals.decode_signal(data, 12, 8, true, false), 0xDB);
-	test.equals(signals.decode_signal(data, 12, 12, true, false), 0xEDB);
-	test.equals(signals.decode_signal(data, 12, 20, true, false), 0xFEEDB);
+	test.equals(signals.decode_signal(data, 12, 8, true, false), 0xEA);
+	test.equals(signals.decode_signal(data, 12, 12, true, false), 0xBEA);
+	test.equals(signals.decode_signal(data, 12, 20, true, false), 0xEFBEA);
 	
-	test.equals(signals.decode_signal(data, 0, 1, true, false), 1);
+	test.equals(signals.decode_signal(data, 0, 1, true, false), 0);
 	test.equals(signals.decode_signal(data, 1, 1, true, false), 1);
-	test.equals(signals.decode_signal(data, 2, 1, true, false), 0);
+	test.equals(signals.decode_signal(data, 2, 1, true, false), 1);
 	test.equals(signals.decode_signal(data, 3, 1, true, false), 1);
 	
 	test.done();
@@ -77,13 +77,13 @@ exports['big_endian_encode'] = function(test) {
 	signals.encode_signal(data, 3, 1, false, false, 1);
 	test.deepEqual(data, new Buffer([0xD0, 0x00, 0x00, 0, 0, 0, 0, 0]));
 	
-	signals.encode_signal(data, 11, 8, false, false, 0xEA);
+	signals.encode_signal(data, 4, 8, false, false, 0xEA);
 	test.deepEqual(data, new Buffer([0xDE, 0xA0, 0x00, 0, 0, 0, 0, 0]));
 	
-	signals.encode_signal(data, 23, 12, false, false, 0xDBE);
+	signals.encode_signal(data, 12, 12, false, false, 0xDBE);
 	test.deepEqual(data, new Buffer([0xDE, 0xAD, 0xBE, 0, 0, 0, 0, 0]));
-	
-	signals.encode_signal(data, 23, 12, false, false, 0);
+
+	signals.encode_signal(data, 12, 12, false, false, 0);
 	test.deepEqual(data, new Buffer([0xDE, 0xA0, 0x00, 0, 0, 0, 0, 0]), "Overwriting signal value failed");
 	
 	test.done();
@@ -91,9 +91,12 @@ exports['big_endian_encode'] = function(test) {
 
 exports['big_endian_decode'] = function(test) {
 	data = new Buffer([0xDE, 0xAD, 0xBE, 0xEF, 0xCA, 0xFE, 0xBA, 0xBE]);
-	
-	test.equals(signals.decode_signal(data, 7, 8, false, false), 0xDE);
-	test.equals(signals.decode_signal(data, 15, 16, false, false), 0xDEAD);
+
+	test.equals(signals.decode_signal(data, 0, 8, false, false), 0xDE);
+	test.equals(signals.decode_signal(data, 0, 16, false, false), 0xDEAD);
+
+	test.equals(signals.decode_signal(data, 7, 8, false, false), 0x56);
+	test.equals(signals.decode_signal(data, 15, 16, false, false), 0xDF77);
 	
 	test.equals(signals.decode_signal(data, 0, 1, false, false), 1);
 	test.equals(signals.decode_signal(data, 1, 1, false, false), 1);
@@ -106,13 +109,13 @@ exports['big_endian_decode'] = function(test) {
 exports['big_endian_signed_encode'] = function(test) {
 	data = new Buffer([0, 0, 0, 0, 0, 0, 0, 0]);
 	
-	signals.encode_signal(data, 7, 8, false, true, -1);
+	signals.encode_signal(data, 0, 8, false, true, -1);
 	test.deepEqual(data, new Buffer([0xFF, 0x00, 0x00, 0, 0, 0, 0, 0]));
 	
-	signals.encode_signal(data, 15, 16, false, true, -2);
+	signals.encode_signal(data, 0, 16, false, true, -2);
 	test.deepEqual(data, new Buffer([0xFF, 0xFE, 0x00, 0, 0, 0, 0, 0]));
 	
-	signals.encode_signal(data, 23, 8, false, true, -128);
+	signals.encode_signal(data, 16, 8, false, true, -128);
 	test.deepEqual(data, new Buffer([0xFF, 0xFE, 0x80, 0, 0, 0, 0, 0]));
 	
 	test.done();
@@ -121,9 +124,9 @@ exports['big_endian_signed_encode'] = function(test) {
 exports['big_endian_signed_decode'] = function(test) {
 	data = new Buffer([0xFF, 0xFE, 0x80 ]);
 	
-	test.equals(signals.decode_signal(data, 7, 8, false, true), -1);
-	test.equals(signals.decode_signal(data, 15, 16, false, true), -2);
-	test.equals(signals.decode_signal(data, 23, 8, false, true), -128);
+	test.equals(signals.decode_signal(data, 0, 8, false, true), -1);
+	test.equals(signals.decode_signal(data, 0, 16, false, true), -2);
+	test.equals(signals.decode_signal(data, 16, 8, false, true), -128);
 	
 	test.done();
 }
