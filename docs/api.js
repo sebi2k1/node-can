3c175fa2828b822af65a6c54ac128af2e951492a
sebi2k1 <dev@sebastianhaas.info>
2016-02-14 16:44:44 +0100

    Updated yuidoc

--- docs/api.js
+++ docs/api.js
@@ -3,25 +3,19 @@ YUI.add("yuidoc-meta", function(Y) {
     "classes": [
         "DatabaseService",
         "Message",
-        "RawChannel",
         "Signal",
         "exports"
     ],
     "modules": [
-        "CAN",
         "Signals"
     ],
     "allModules": [
-        {
-            "displayName": "CAN",
-            "name": "CAN",
-            "description": "Basic CAN access"
-        },
         {
             "displayName": "Signals",
             "name": "Signals",
             "description": "The Signals modules provides an interface to access the values/signals\nencoded in CAN messages."
         }
-    ]
+    ],
+    "elements": []
 } };
 });
