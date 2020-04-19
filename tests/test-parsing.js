52dc4d6e4382d2a924c3b8767a2d609ac1b354bb
Sebastian Haas <dev@sebastianhaas.info>
2020-04-19 14:21:34 +0000

    Added test for muxgroup issue #80

--- tests/test-parsing.js
+++ tests/test-parsing.js
@@ -4,6 +4,10 @@ exports['test_sample_consumes'] = function(test) {
 	// Parse database
 	var network = can.parseNetworkDescription("./tests/samples.kcd");
 
+	test.equals(network.buses['OBD2'].messages[0].signals[0].mux, 0)
+	test.equals(network.buses['OBD2'].messages[0].muxed, true)
+	test.equals(network.buses['OBD2'].messages[0].name, 'OBD2')
+
 	test.equals(network.nodes['12'].buses['Motor'].consumes[0].id, 895);
 
 	test.done();
