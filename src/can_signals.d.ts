6df16c925f8dc46e0befbb613d9617c28c89df09
Joshua Chaitin-Pollak <josh@c11k.org>
2022-08-28 07:26:28 -0400

    code compiles

--- src/can_signals.d.ts
+++ src/can_signals.d.ts
@@ -1,33 +1,33 @@
-declare module "can_signals" {
-    // Decode signal according description
-    // arg[0] - Data array
-    // arg[1] - offset zero indexed
-    // arg[2] - bitLength one indexed
-    // arg[3] - endianess
-    // arg[4] - signed flag
-    function decodeSignal(
-        data: Buffer,
-        bitOffset: number,
-        bitLength: number,
-        endianess: boolean,
-        signed: boolean,
-    ): number[];
+declare module "*can_signals.node" {
+	// Decode signal according description
+	// arg[0] - Data array
+	// arg[1] - offset zero indexed
+	// arg[2] - bitLength one indexed
+	// arg[3] - endianess
+	// arg[4] - signed flag
+	function decodeSignal(
+		data: Buffer,
+		bitOffset: number,
+		bitLength: number,
+		endianess: boolean,
+		signed: boolean
+	): number[];
 
-    // Encode signal according description
-    // arg[0] - Data array
-    // arg[1] - bitOffset
-    // arg[2] - bitLength
-    // arg[3] - endianess
-    // arg[4] - signed flag
-    // arg[5] - first 4 bytes value to encode
-    // arg[6] - second 4 bytes value to encode
-    function encodeSignal(
-        data: Buffer,
-        bitOffset: number,
-        bitLength: number,
-        endianess: boolean,
-        signed: boolean,
-        word1: number | boolean,
-        word2?: number | boolean,
-    ): void;
+	// Encode signal according description
+	// arg[0] - Data array
+	// arg[1] - bitOffset
+	// arg[2] - bitLength
+	// arg[3] - endianess
+	// arg[4] - signed flag
+	// arg[5] - first 4 bytes value to encode
+	// arg[6] - second 4 bytes value to encode
+	function encodeSignal(
+		data: Buffer,
+		bitOffset: number,
+		bitLength: number,
+		endianess: boolean,
+		signed: boolean,
+		word1: number | boolean,
+		word2?: number | boolean
+	): void;
 }
--- src/parse_kcd.ts
+++ src/parse_kcd.ts
@@ -19,314 +19,360 @@
  * IN THE SOFTWARE.
  */
 
-import * as fs from 'fs';
-import * as xml2js from 'xml2js';
+import * as fs from "fs";
+import * as xml2js from "xml2js";
 
 export class J1939 {
-    constructor(
-        public AAC: number,
-        public Function: number,
-        public Vehicle: number,
-        public Identity: number,
-        public Industry: number,
-        public System: number,
-        public Manufacture: number,
-    ) {}
+	constructor(
+		public AAC: number,
+		public Function: number,
+		public Vehicle: number,
+		public Identity: number,
+		public Industry: number,
+		public System: number,
+		public Manufacture: number
+	) {}
 
-    getName() {
-        var name = Buffer.alloc(8);
-        name[7] = ((this.AAC & 0x1) << 7) | ((this.Industry & 0x7) << 4) | (this.Vehicle & 0xF);
-        name[6] = (this.System) << 1  & 0xFE;
-        name[5] = this.Function & 0xFF;
-        name[4] = 0; // function Instance & ECU instance
-        name[3] = (this.Manufacture >> 3) & 0xFF ;
-        name[2] = ((this.Manufacture & 0x7) << 5) | ( (this.Identity >> 16) & 0x1F );
-        name[1] = (this.Identity >> 8 ) & 0xFF;
-        name[0] = this.Identity & 0xFF;
+	getName() {
+		const name = Buffer.alloc(8);
+		name[7] =
+			((this.AAC & 0x1) << 7) |
+			((this.Industry & 0x7) << 4) |
+			(this.Vehicle & 0xf);
+		name[6] = (this.System << 1) & 0xfe;
+		name[5] = this.Function & 0xff;
+		name[4] = 0; // function Instance & ECU instance
+		name[3] = (this.Manufacture >> 3) & 0xff;
+		name[2] = ((this.Manufacture & 0x7) << 5) | ((this.Identity >> 16) & 0x1f);
+		name[1] = (this.Identity >> 8) & 0xff;
+		name[0] = this.Identity & 0xff;
 
-        return name;
-    }
+		return name;
+	}
+}
+
+interface SignalRef {
+	id: number;
+	signal_name: string;
 }
 
 export class BusRefs {
-    public produces: number[] = [];
-    public consumes: number[] = [];
+	public produces: number[] = [];
+	public consumes: SignalRef[] = [];
 }
 
 export class Node {
-    public buses: Record<string, BusRefs> = {};
+	public buses: Record<string, BusRefs> = {};
 
-    constructor(
-        public id: number,
-        public name: string,
-        public device: object,
-        public j1939: J1939,
-    ) {}
+	constructor(
+		public id: number,
+		public name: string,
+		public device: object,
+		public j1939: J1939
+	) {}
 }
 
 export class Value {
-    constructor(
-        public slope: number = 1.0,
-        public intercept: number = 0.0,
-        public unit: string = "",
-        public type: 'signed' | 'unsigned' = 'unsigned',
-        public defaultValue: number = 0.0,
-        public minValue?: number,
-        public maxValue?: number,
-    ) {}
+	constructor(
+		public slope: number = 1.0,
+		public intercept: number = 0.0,
+		public unit: string = "",
+		public type: "signed" | "unsigned" = "unsigned",
+		public defaultValue: number = 0.0,
+		public minValue?: number,
+		public maxValue?: number
+	) {}
 }
 
 export class Signal {
-    constructor(
-        public name: string,
-        public spn: string,
-        public bitOffset: number,
-        public bitLength: number,
-        public endianess: 'little' | 'big',
-        public labels: Record<number, string>,
-        public value?: Value,
-    ) {}
+	constructor(
+		public name: string,
+		public spn: string,
+		public bitOffset: number,
+		public bitLength: number,
+		public endianess: "little" | "big",
+		public labels: Record<number, string>,
+		public mux: number,
+		public value?: Value
+	) {}
 }
 
 export class NodeRef {
-    constructor(
-        public id: number
-    ) {}
+	constructor(public id: number) {}
+}
+
+export class Mux {
+	constructor(
+		public name: string,
+		public offset: number,
+		public length: number
+	) {}
 }
 
 export class Message {
-    public signals: Signal[] = [];
-    public producers: NodeRef[] = [];
+	public signals: Signal[] = [];
+	public producers: NodeRef[] = [];
 
-    constructor(
-        public name: string,
-        public id: number,
-        public ext: boolean,
-        public triggered: boolean,
-        public length: number,
-        public interval: number,
-        public muxed: boolean,
-        public mux: undefined
-    ) {}
+	constructor(
+		public name: string,
+		public id: number,
+		public ext: boolean,
+		public triggered: boolean,
+		public length: number,
+		public interval: number,
+		public muxed: boolean,
+		public mux?: Mux
+	) {}
 }
 
 export class Bus {
-    public messages: Message[] = [];
-
-    constructor() {}
-
+	public messages: Message[] = [];
 }
 
 export class CanNetwork {
-    public nodes: Record<string, Node> = {};
-    public buses: Record<string, Bus> = {};
+	public nodes: Record<string, Node> = {};
+	public buses: Record<string, Bus> = {};
 }
 
-//-----------------------------------------------------------------------------
-function makeSignalFromXml(xmlSignal: Record<string, any>, xmlValue: any, labelset: any): Signal {
-    const bitOffset = parseInt(xmlSignal.offset);
+interface XmlSignalDef {
+	name: string;
+	spn: string;
+	length: string;
+	offset: string;
+	endianess: "big" | "little";
+}
 
-    const labelSet: Record<number, string> = {};
-    // add label sets from the database.
-    if (Array.isArray(labelset)) {
-        var rawLabels = labelset[0]['Label'];
-        if (rawLabels != undefined) {
-            rawLabels.forEach((l: any) => {
-                labelSet[l['$'].value] = l['$'].name;
-            });
-        }
-    }
+// -----------------------------------------------------------------------------
+function makeSignalFromXml(
+	xmlSignal: XmlSignalDef,
+	xmlValue: Record<string, unknown>,
+	labelset: Record<string, unknown>,
+	muxCount: number
+): Signal {
+	const bitOffset = parseInt(xmlSignal.offset);
 
-    let value: Value | undefined;
-    // add Values from the database
-    if (Array.isArray(xmlValue)) {
-        const rawValue = xmlValue[0]['$'];
+	const labelSet: Record<number, string> = {};
+	// add label sets from the database.
+	if (Array.isArray(labelset)) {
+		const rawLabels = labelset[0]["Label"];
+		if (rawLabels != undefined) {
+			rawLabels.forEach((l: any) => {
+				labelSet[l["$"].value] = l["$"].name;
+			});
+		}
+	}
 
-        value = new Value(
-            rawValue.slope ? parseFloat(rawValue.slope) : 1.0,
-            rawValue.intercept ? parseFloat(rawValue.intercept) : 0.0,
-            rawValue.unit ? rawValue.unit : "",
-            rawValue.type ? rawValue.type : "unsigned",
-            rawValue.defaultValue ? parseFloat(rawValue.defaultValue) : 0.0,
-            rawValue.min ? rawValue.min : undefined,
-            rawValue.max ? rawValue.max : undefined,
-        );
-    }
+	let value: Value | undefined;
+	// add Values from the database
+	if (Array.isArray(xmlValue)) {
+		const rawValue = xmlValue[0]["$"];
 
-    const newSignal = new Signal(
-        xmlSignal.name,
-        xmlSignal.spn,
-        bitOffset,
-        xmlSignal.length ? parseInt(xmlSignal.length) : 1,
-        xmlSignal.endianess ? xmlSignal.endianess : 'little',
-        labelSet,
-        value,
-    );
+		value = new Value(
+			rawValue.slope ? parseFloat(rawValue.slope) : 1.0,
+			rawValue.intercept ? parseFloat(rawValue.intercept) : 0.0,
+			rawValue.unit ? rawValue.unit : "",
+			rawValue.type ? rawValue.type : "unsigned",
+			rawValue.defaultValue ? parseFloat(rawValue.defaultValue) : 0.0,
+			rawValue.min ? rawValue.min : undefined,
+			rawValue.max ? rawValue.max : undefined
+		);
+	}
 
-    return newSignal
+	const newSignal = new Signal(
+		xmlSignal.name,
+		xmlSignal.spn,
+		bitOffset,
+		xmlSignal.length ? parseInt(xmlSignal.length) : 1,
+		xmlSignal.endianess ? xmlSignal.endianess : "little",
+		labelSet,
+		muxCount,
+		value
+	);
+
+	return newSignal;
 }
 
 export function parseKcdFile(file: fs.PathOrFileDescriptor) {
-    // Result will be a dictionary describing the whole network
-    const network = new CanNetwork();
+	// Result will be a dictionary describing the whole network
+	const network = new CanNetwork();
 
-    const data = fs.readFileSync(file);
+	const data = fs.readFileSync(file);
 
-    const parser = new xml2js.Parser({explicitArray: true});
+	const parser = new xml2js.Parser({ explicitArray: true });
 
-    parser.parseString(data, function(e, parsed) {
-        const networkDefinition = parsed['NetworkDefinition'];
+	parser.parseString(data, function (e, parsed) {
+		const networkDefinition = parsed["NetworkDefinition"];
 
-        for (const n in networkDefinition['Node']) {
-            const rawNode = networkDefinition['Node'][n]['$'];
+		for (const n in networkDefinition["Node"]) {
+			const rawNode = networkDefinition["Node"][n]["$"];
 
-            const newNode = new Node(
-                rawNode['id'],
-                rawNode['name'],
-                rawNode['device'],
-                new J1939(
-                    rawNode['J1939AAC'],
-                    rawNode['J1939Function'],
-                    rawNode['J1939Vehicle'],
-                    rawNode['J1939IdentityNumber'],
-                    rawNode['J1939IndustryGroup'],
-                    rawNode['J1939System'],
-                    rawNode['J1939ManufacturerCode'],
-                )
-            )
+			const newNode = new Node(
+				rawNode["id"],
+				rawNode["name"],
+				rawNode["device"],
+				new J1939(
+					rawNode["J1939AAC"],
+					rawNode["J1939Function"],
+					rawNode["J1939Vehicle"],
+					rawNode["J1939IdentityNumber"],
+					rawNode["J1939IndustryGroup"],
+					rawNode["J1939System"],
+					rawNode["J1939ManufacturerCode"]
+				)
+			);
 
-            network.nodes[rawNode['id']] = newNode;
-        }
+			network.nodes[rawNode["id"]] = newNode;
+		}
 
-        for (const b in networkDefinition['Bus']) {
-            const rawBus = networkDefinition['Bus'][b]['$'];
+		for (const b in networkDefinition["Bus"]) {
+			const rawBus = networkDefinition["Bus"][b]["$"];
 
-            const busName: string = rawBus['name'];
-            const newBus = new Bus();
+			const busName: string = rawBus["name"];
+			const newBus = new Bus();
 
-            const rawBusMessages = networkDefinition['Bus'][b]['Message'];
-            for (const messageKey in rawBusMessages) {
-                const rawMessage = rawBusMessages[messageKey];
-                const message = rawMessage['$'];
-                const producers = rawMessage['Producer'];
-                const multiplex = rawMessage['Multiplex'];
+			const rawBusMessages = networkDefinition["Bus"][b]["Message"];
+			for (const messageKey in rawBusMessages) {
+				const rawMessage = rawBusMessages[messageKey];
+				const message = rawMessage["$"];
+				const producers = rawMessage["Producer"];
+				const multiplex = rawMessage["Multiplex"];
 
-                var newMessage = new Message(
-                    message.name,
-                    parseInt(message.id, 16),
-                    message.format == 'extended',
-                    message.triggered == 'true',
-                    message.length ? parseInt(message.length) : 0,
-                    message.interval ? parseInt(message.interval) : 0,
-                    (multiplex != undefined),
-                    undefined
-                )
+				const newMessage = new Message(
+					message.name,
+					parseInt(message.id, 16),
+					message.format == "extended",
+					message.triggered == "true",
+					message.length ? parseInt(message.length) : 0,
+					message.interval ? parseInt(message.interval) : 0,
+					multiplex != undefined
+				);
 
-                // Add messages going out and from whom.
-                for (const p in producers) {
-                    const nodeRefs = producers[p]['NodeRef'];
-                    for (const n in nodeRefs) {
-                        const nodeRefId: number = nodeRefs[n]['$']['id'];
+				// Add messages going out and from whom.
+				for (const p in producers) {
+					const nodeRefs = producers[p]["NodeRef"];
+					for (const n in nodeRefs) {
+						const nodeRefId: number = nodeRefs[n]["$"]["id"];
 
-                        message.producers.push(new NodeRef(nodeRefId));
+						message.producers.push(new NodeRef(nodeRefId));
 
-                        // Look up the node by _id_ (number), not name.
-                        const nodeDef: Node = network.nodes[nodeRefId];
+						// Look up the node by _id_ (number), not name.
+						const nodeDef: Node = network.nodes[nodeRefId];
 
-                        if (nodeDef)
-                        {
-                            const bus = nodeDef.buses[busName];
-                            bus.produces.push(newMessage.id);
-                        }
-                    }
-                }
+						if (nodeDef) {
+							const bus = nodeDef.buses[busName];
+							bus.produces.push(newMessage.id);
+						}
+					}
+				}
 
-                if (!newMessage.interval)
-                    newMessage.interval = 0;
+				if (!newMessage.interval) newMessage.interval = 0;
 
-                var maxOffset = 0;
+				let maxOffset = 0;
 
-                // look for multiplexed messages
-                for (const mux in multiplex) {
-                    newMessage.mux = {
-                        name: multiplex[mux]['$']['name'],
-                        offset: parseInt(multiplex[mux]['$']['offset']),
-                        length: parseInt(multiplex[mux]['$']['length'])
-                    }
+				// look for multiplexed messages
+				for (const mux in multiplex) {
+					newMessage.mux = {
+						name: multiplex[mux]["$"]["name"],
+						offset: parseInt(multiplex[mux]["$"]["offset"]),
+						length: parseInt(multiplex[mux]["$"]["length"]),
+					};
 
-                    for (const mg in multiplex[mux]['MuxGroup']) {
-                        var muxmsg = multiplex[mux]['MuxGroup'][mg]['$'];
+					for (const mg in multiplex[mux]["MuxGroup"]) {
+						const muxmsg = multiplex[mux]["MuxGroup"][mg]["$"];
 
-                        for (const s in multiplex[mux]['MuxGroup'][mg]['Signal']) {
-                            var signal = multiplex[mux]['MuxGroup'][mg]['Signal'][s]['$'];
-                            var value = multiplex[mux]['MuxGroup'][mg]['Signal'][s]['Value'];
-                            var labelset = multiplex[mux]['MuxGroup'][mg]['Signal'][s]['LabelSet'];
+						for (const s in multiplex[mux]["MuxGroup"][mg]["Signal"]) {
+							const signal = multiplex[mux]["MuxGroup"][mg]["Signal"][s]["$"];
+							const value =
+								multiplex[mux]["MuxGroup"][mg]["Signal"][s]["Value"];
+							const labelset =
+								multiplex[mux]["MuxGroup"][mg]["Signal"][s]["LabelSet"];
 
-                            var newSignal = makeSignalFromXml(signal, value, labelset)
+							// Added multiplexor
+							const muxCount = parseInt(muxmsg["count"]);
 
-                            // Added multiplexor
-                            newSignal.mux = parseInt(muxmsg['count'])
+							const newSignal = makeSignalFromXml(
+								signal,
+								value,
+								labelset,
+								muxCount
+							);
 
-                            newMessage.signals.push(newSignal);
+							newMessage.signals.push(newSignal);
 
-                            var offset_num = newSignal.bitOffset + newSignal.bitLength;
+							const offset_num = newSignal.bitOffset + newSignal.bitLength;
 
-                            if (offset_num > maxOffset)
-                                maxOffset = offset_num;
-                        }
-                    }
+							if (offset_num > maxOffset) maxOffset = offset_num;
+						}
+					}
 
-                    // only one muxer supported right now
-                    break
-                }
+					// only one muxer supported right now
+					break;
+				}
 
-                for (const s in networkDefinition['Bus'][b]['Message'][messageKey]['Signal']) {
-                    var signal = networkDefinition['Bus'][b]['Message'][messageKey]['Signal'][s]['$'];
-                    var value = networkDefinition['Bus'][b]['Message'][messageKey]['Signal'][s]['Value'];
-                    var labelset = networkDefinition['Bus'][b]['Message'][messageKey]['Signal'][s]['LabelSet'];
-                    var consumers = networkDefinition['Bus'][b]['Message'][messageKey]['Signal'][s]['Consumer'];
+				for (const s in networkDefinition["Bus"][b]["Message"][messageKey][
+					"Signal"
+				]) {
+					const signal =
+						networkDefinition["Bus"][b]["Message"][messageKey]["Signal"][s][
+							"$"
+						];
+					const value =
+						networkDefinition["Bus"][b]["Message"][messageKey]["Signal"][s][
+							"Value"
+						];
+					const labelset =
+						networkDefinition["Bus"][b]["Message"][messageKey]["Signal"][s][
+							"LabelSet"
+						];
+					const consumers =
+						networkDefinition["Bus"][b]["Message"][messageKey]["Signal"][s][
+							"Consumer"
+						];
 
-                    var newSignal = makeSignalFromXml(signal, value, labelset)
+					const newSignal = makeSignalFromXml(signal, value, labelset, 0);
 
-                    // Add listeners / targets for the message.
-                    for (const c in consumers) {
-                        for (const n in consumers[c]['NodeRef']) {
-                            var id = consumers[c]['NodeRef'][n]['$']['id'];
+					// Add listeners / targets for the message.
+					for (const c in consumers) {
+						for (const n in consumers[c]["NodeRef"]) {
+							const id = consumers[c]["NodeRef"][n]["$"]["id"];
 
-                            if (network.nodes[id])
-                            {
-                                if (network.nodes[id].buses[busName] == undefined)
-                                    network.nodes[id].buses[busName] = { produces: [], consumes: []}
+							if (network.nodes[id]) {
+								if (network.nodes[id].buses[busName] == undefined)
+									network.nodes[id].buses[busName] = {
+										produces: [],
+										consumes: [],
+									};
 
-                                network.nodes[id].buses[busName].consumes.push({ id: newMessage.id, signal_name: newSignal.name });
-                            }
-                        }
-                    }
+								network.nodes[id].buses[busName].consumes.push({
+									id: newMessage.id,
+									signal_name: newSignal.name,
+								});
+							}
+						}
+					}
 
-                    newMessage.signals.push(newSignal);
+					newMessage.signals.push(newSignal);
 
-                    var offset_num = newSignal.bitOffset + newSignal.bitLength;
+					const offset_num = newSignal.bitOffset + newSignal.bitLength;
 
-                    if (offset_num > maxOffset)
-                        maxOffset = offset_num;
-                }
+					if (offset_num > maxOffset) maxOffset = offset_num;
+				}
 
-                // Calculate length based on define signals
-                if (!newMessage.length) {
-                    newMessage.length = maxOffset / 8;
+				// Calculate length based on define signals
+				if (!newMessage.length) {
+					newMessage.length = maxOffset / 8;
 
-                    if (maxOffset % 8 > 0)
-                        newMessage.length++;
-                }
+					if (maxOffset % 8 > 0) newMessage.length++;
+				}
 
-                newBus.messages.push(newMessage);
-            }
+				newBus.messages.push(newMessage);
+			}
 
-            network.buses[busName] = newBus;
-        }
-    });
+			network.buses[busName] = newBus;
+		}
+	});
 
-    // NOTE: Not sure if it is safe here to access result, but I guess parsing the XML file is more or less synchronous.
+	// NOTE: Not sure if it is safe here to access result, but I guess parsing the XML file is more or less synchronous.
 
-    return network;
+	return network;
 }
